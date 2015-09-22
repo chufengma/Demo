e22c0e1a23882bec2486493c085ec28fb2b80eb0
chufengma <chufengma@foxmail.com>
2015-09-22 22:13:22 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	androidprac/androidprac.iml
    #	androidprac/src/main/res/values/colors.xml
    #	androidprac/src/main/res/values/strings.xml
    #	batter/batter.iml

---
+++ androidprac/src/main/java/com/onfengma/androidprac/DrawerLayoutActivity.java
@@ -0,0 +1,48 @@
+package com.onfengma.androidprac;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class DrawerLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawer_layout);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_awesome_toolbar);
+        setSupportActionBar(toolbar);
+
+        // 注意setStatusBarBackgroundColor方法需要你将fitsSystemWindows设置为true才会生效
+        // DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        // drawerLayout.setStatusBarBackgroundColor(getResources().getColor(R.color.dialog_bg));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_drawer_layout, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
